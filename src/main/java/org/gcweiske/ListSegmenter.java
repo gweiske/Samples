ee208f90c55a25da1869998e0c8ee157de78690d
Gregor Weiske <gcweiske@gmail.com>
2020-05-19 08:47:02 -0400

    get sequential days from list

---
+++ src/main/java/org/gcweiske/ListSegmenter.java
@@ -0,0 +1,38 @@
+package org.gcweiske;
+
+import java.time.LocalDate;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
+public class ListSegmenter {
+    private Map<LocalDate, Integer> map;
+
+    public ListSegmenter() {
+    }
+
+    public Map<LocalDate, Integer> getMap(List<LocalDate> dates) {
+        Iterator<LocalDate> iterator = dates.stream().sorted(LocalDate::compareTo).iterator();
+        map = new HashMap<>();
+        if (iterator.hasNext()) {
+            LocalDate date = iterator.next();
+            map.put(date, 1);
+            segment(iterator, date, date, map);
+        }
+        return map;
+    }
+
+    public void segment(Iterator<LocalDate> iterator, LocalDate previousDate, LocalDate mapKey, Map<LocalDate, Integer> map) {
+        if (iterator.hasNext()) {
+            LocalDate currentDate = iterator.next();
+            if (previousDate.plusDays(1).equals(currentDate)) {
+                map.put(mapKey, map.get(mapKey) + 1);
+                segment(iterator, currentDate, mapKey, map);
+            } else {
+                map.put(currentDate, 1);
+                segment(iterator, currentDate, currentDate, map);
+            }
+        }
+    }
+}
---
+++ src/test/java/org/gcweiske/ListSegmenterTest.java
@@ -0,0 +1,45 @@
+package org.gcweiske;
+
+import org.junit.Test;
+
+import java.time.LocalDate;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class ListSegmenterTest {
+    @Test
+    public void testIt() {
+        LocalDate today = LocalDate.now();
+        List<LocalDate> dates = List.of(
+                today,
+                today.plusDays(2),
+                today.plusDays(3),
+                today.minusDays(2),
+                today.minusDays(3),
+                today.minusDays(5)
+        );
+        ListSegmenter listSegmenter = new ListSegmenter();
+        List<LocalDate> dateList = dates.stream().sorted(LocalDate::compareTo).collect(Collectors.toList());
+        System.out.println(dateList);
+        System.out.println(listSegmenter.getMap(dates));
+
+        dates = List.of();
+        dateList = dates.stream().sorted(LocalDate::compareTo).collect(Collectors.toList());
+        System.out.println(dateList);
+        System.out.println(listSegmenter.getMap(dates));
+
+        dates = List.of(
+                today,
+                today.minusDays(1),
+                today.minusDays(2),
+                today.minusDays(3),
+                today.minusDays(4),
+                today.minusDays(5)
+        );
+        dateList = dates.stream().sorted(LocalDate::compareTo).collect(Collectors.toList());
+        System.out.println(dateList);
+        System.out.println(listSegmenter.getMap(dates));
+    }
+}
